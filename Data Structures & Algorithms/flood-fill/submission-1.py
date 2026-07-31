14e669a2b1a6314f1702ff57f319045ae1dad0b0
Jincheng <83278146+jma49@users.noreply.github.com>
2026-07-31 15:21:26 -0700

    Add: flood-fill - submission-1

---
+++ Data Structures & Algorithms/flood-fill/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        # BFS
+        rows, cols = len(image), len(image[0])
+        origin_color = image[sr][sc]
+        
+        if origin_color == color:
+            return image
+
+        queue = deque([(sr, sc)])
+        image[sr][sc] = color
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        while queue:
+            r, c = queue.popleft()
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and image[nr][nc] == origin_color:
+                        image[nr][nc] = color
+                        queue.append((nr, nc))
+        
+        return image
